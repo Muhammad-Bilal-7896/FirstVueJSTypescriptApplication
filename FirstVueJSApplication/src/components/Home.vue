3deebfebdf6b4ee001deff83b8aa0f72769da5cd
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2021-09-25 03:17:17 +0500

    feat

--- FirstVueJSApplication/src/components/Home.vue
+++ FirstVueJSApplication/src/components/Home.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="home">
         <h1>{{ msg }}</h1>
+        <p>This is my first VUE Js Application for testing purposes</p>
         <p>Welcome to your new single-page application, built with <a href="https://vuejs.org" target="_blank">Vue.js</a> and <a href="http://www.typescriptlang.org/" target="_blank">TypeScript</a>.</p>
     </div>
 </template>
